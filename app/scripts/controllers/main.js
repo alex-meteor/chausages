ad9c83971d49a9793c5c49fa4a8cee54a32f33ca
Griffith Pickett <pickett65@gmail.com>
2014-07-19 11:11:46 -0500

    init commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('chasaugeApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  });
