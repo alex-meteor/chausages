c72fd01ce7309a1a3c30ca9a1be07ba903532ad0
Dustin Hudson <dhudson@blackhawksap.com>
2014-07-19 17:07:17 -0500

    base: song directive

---
+++ app/scripts/directives/song.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name chasaugeApp.directive:song
+ * @description
+ * # song
+ */
+angular.module('app')
+  .directive('song', function () {
+    return {
+      templateUrl: 'partials/song.html',
+      restrict: 'E',
+	    scope: {
+		    art: '=',
+		    name: '@',
+		    extra: '@'
+	    },
+      link: function postLink(scope, element, attrs) {
+				$scope.toggle = { info: false };
+      }
+    };
+  });
