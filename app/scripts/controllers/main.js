0fa5ebcec9c142075054bf60961483495865f80a
Griffith Pickett <pickett65@gmail.com>
2014-07-19 14:50:23 -0500

    add client socket.io

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('chasaugeApp')
-  .controller('MainCtrl', function ($scope, $http) {
+  .controller('MainCtrl', function ($scope, $http, socket) {
     $http.get('/api/awesomeThings').success(function(awesomeThings) {
       $scope.awesomeThings = awesomeThings;
     });
--- app/scripts/services/socket.js
+++ app/scripts/services/socket.js
@@ -1,16 +1,6 @@
 'use strict';
 
 angular.module('chasaugeApp')
-  .factory('socket', function () {
-    // Service logic
-    // ...
-
-    var meaningOfLife = 42;
-
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
-  });
+.factory('socket', function (socketFactory) {
+	return socketFactory();
+});
