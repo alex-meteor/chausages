c0783af80d5ba18316d7186eee51cb5996986e17
Dustin Hudson <dhudson@blackhawksap.com>
2014-07-19 22:54:43 -0500

    service: queue

--- app/scripts/controllers/queue.js
+++ app/scripts/controllers/queue.js
@@ -1,25 +1,19 @@
 'use strict';
 
 angular.module('app')
-  .controller('QueueController', function ($scope, $http, Track, socket) {
+  .controller('QueueController', function ($scope, $http, Queue, socket) {
 		var _this = this;
-		_this.list = [];
+		_this.list = Queue.list;
 
 		//-- START MOCKS --//
-		for(var i = 0; i < mocks.tracks.length; i++) { mocks.tracks[i] = new Track(mocks.tracks[i]); }
-		_this.list = mocks.tracks;
+		Queue.load(mocks.tracks);
 		_this.playing = this.list.pop();
 		//-- END MOCKS --//
 
 		/*
 		*	handle socket emissions
 		*/
-		socket.forward('welcome', $scope);
-		socket.forward('update:queue', $scope);
-		$scope.$on('socket:welcome', function (env, data) {
-			socket.emit('queue:add', { song: 'Dr. Carter' });
-		});
-		$scope.$on('queue:add', function (env, data){
-			_this.list.push(new Track(data));
+		socket.on('welcome', function(e) {
+			console.log(e.msg);
 		});
   });
