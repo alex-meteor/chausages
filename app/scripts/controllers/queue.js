1acf312795187f35f140f6daa837a871d7fc5135
Dustin Hudson <dhudson@blackhawksap.com>
2014-07-19 20:14:35 -0500

    queue stuff

--- app/scripts/controllers/queue.js
+++ app/scripts/controllers/queue.js
@@ -1,62 +1,24 @@
 'use strict';
 
 angular.module('app')
-  .controller('QueueController', function ($scope, $http, socket) {
-		this.list = [ {
-			art: 'http://placehold.it/150x150',
-			name: 'Song Name',
-			user: { name: 'Jae Cha' },
-			votes: [
-				{vote: true}, {vote: true}, {vote: true}, {vote: true}, {vote: false}
-			],
-			voted: true
-		},
-		{
-			art: 'http://placehold.it/150x150',
-			name: 'Song Name',
-			user: { name: 'Jae Cha' },
-			voted: false
-		},
-		{
-			art: 'http://placehold.it/150x150',
-			name: 'Song Name',
-			user: { name: 'Jae Cha' },
-			voted: false
-		},
-		{
-			art: 'http://placehold.it/150x150',
-			name: 'Song Name',
-			user: { name: 'Jae Cha' },
-			voted: false
-		},
-		{
-			art: 'http://placehold.it/150x150',
-			name: 'Song Name',
-			user: { name: 'Jae Cha' },
-			voted: false
-		}
+  .controller('QueueController', function ($scope, $http, Track, socket) {
+		this.list = [];
 
-	];
+		//-- START MOCKS --//
+		for(var i = 0; i < mocks.tracks.length; i++) { mocks.tracks[i] = new Track(mocks.tracks[i]); }
+		this.list = mocks.tracks;
+		this.playing = this.list.pop();
+		//-- END MOCKS --//
 
-		this.playing = {
-			art: 'http://placehold.it/150x150',
-			name: 'Song Name',
-			user: { name: 'Jae Cha' }
-		};
-
-		this.vote = function(_id, value) {
-			socket.emit('socket:vote', {_id: _id, value: value} );
-		};
 		/*
 		*	handle socket emissions
 		*/
 		socket.forward('welcome', $scope);
 		socket.forward('update:queue', $scope);
 		$scope.$on('socket:welcome', function (env, data) {
-			console.log(data.msg);
 			socket.emit('queue:add', { song: 'Dr. Carter' });
 		});
-		$scope.$on('socket:update:queue', function (env, data){
-			console.log(data.song);
+		$scope.$on('queue:add', function (env, data){
+			this.list.push(new Track(data));
 		});
   });
