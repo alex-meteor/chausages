48d9dbd8bde305da487a42147b3da8ad4b7d4570
Patrick Burns <pburns@kineticsupply.com>
2014-07-20 02:06:36 -0500

    search **SHOULD** work

--- app/js/main.js
+++ app/js/main.js
@@ -27,27 +27,16 @@
 				});
 
 				self.socket.on('queue:vote', function(data) {
-					console.log(data.msg);
+					console.log(data);
+					var fromIndex = 0;
+					var toIndex = 1;
+					R.player.queue.move(fromIndex, toIndex);
 				});
 
-				self.socket.on('service:rdio:search', function(data) {
+				self.socket.on('service:rdio:search:r', function(data) {
+					console.log('recieved search r', data);
 					var results = self.search(data.query);
-					console.log(results);
 				});
-
-				// R.request({
-				// 	method: "search", 
-				// 	content: {
-				// 		query: "science of selling",
-				// 		types: "Track"
-				// 	},
-				// 	success: function(response) {
-				// 		self.showResults(response.result);
-				// 	},
-				// 	error: function(response) {
-				// 		$(".error").text(response.message);
-				// 	}
-				// });
 			});
 		}, 
 		
@@ -62,7 +51,8 @@
 				},
 				success: function(response) {
 					console.log(response.result.results);
-					self.socket.emit("service:rdio:search:results", {"results" : response.result.results});
+					console.log('about to emit results to client');
+					self.socket.emit("service:rdio:search:r:results", {"results" : response.result.results});
 					return response.result.results;
 				},
 				error: function(response) {
@@ -75,29 +65,6 @@
 			var self = this;
 			R.player.queue.add(track.key);
 			R.player.queue.play();
-		},
-		// ----------
-		showResults: function(albums) {
-			console.log('showing results');
-			var self = this;
-			this.$results.empty();
-			console.log(albums);
-			_.each(albums.results, function(album) {
-				console.log(album);
-				album.appUrl = album.shortUrl.replace("http", "rdio");
-				var $el = $('<span class="album">'
-						+'<div class="icon" style="background-image: url(' + album.icon +')">'
-							+'<div class="buttons">'
-							 +'<span class="play btn">Play Album</span>'
-								+'<a href="http://www.rdio.com' + album.url + '" target="_blank" class="site btn">View on Rdio.com</a>'
-								+'<a href="'+ album.appUrl + '" target="_blank" class="app btn">View in Rdio app</a>'
-							+'</div>'
-						+'</div>'
-						+'<div class="title label">' + album.name + '</div>'
-						+'<div class="artist label">' + album.artist + '</div>'
-					+'</span>').appendTo(self.$results);
-				
-			});
 		}
 	};
 	
