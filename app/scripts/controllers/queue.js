1fc8291104f2bb0c79df2a62f98df68ce43a31ab
Dustin Hudson <dhudson@blackhawksap.com>
2014-07-20 03:33:14 -0500

    queue update

--- app/scripts/controllers/queue.js
+++ app/scripts/controllers/queue.js
@@ -3,15 +3,9 @@
 angular.module('app')
   .controller('QueueController', function ($rootScope, $scope, $http, Queue, socket) {
 		var _this = this;
-		// _this.list = Queue.list;
+		_this.list = Queue.list;
 		// //-- START MOCKS --//
 		// Queue.load(mocks.tracks);
 		// _this.playing = this.list.pop();
 		// //-- END MOCKS --//
-
-		$rootScope.$watch("trackQueue", function(queue){
-			console.log(queue);
-			_this.list = queue;
-		});
-
   });
